649f141e4e4c0fac4ca63b311c5874c6911bd935
kalra-rahul <kalrarahul431@gmail.com>
2026-01-31 09:50:31 +0530

    Add Update Item Route

--- src/main/java/com/example/demo/controller/ItemListController.java
+++ src/main/java/com/example/demo/controller/ItemListController.java
@@ -5,10 +5,10 @@ import com.example.demo.service.ItemListService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -24,6 +24,11 @@ public class ItemListController {
 
     @PostMapping("/save-item-list")
     public String saveItemList(@RequestBody ItemList itemListData){
+        return itemListService.saveItemList(itemListData);
+    }
+
+    @PutMapping("/save-item-list")
+    public String updateItemList(@RequestBody ItemList itemListData){
         return itemListService.saveItemList(itemListData);
     }
 }
